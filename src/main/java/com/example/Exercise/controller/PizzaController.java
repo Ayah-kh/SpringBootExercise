03c4c362b5938f2c87d26a21bc999c5967147308
Ayah.kh GitHub <ayah.java.dev@gmail.com>
2023-10-19 12:44:12 +0300

    First Commit

---
+++ src/main/java/com/example/Exercise/controller/PizzaController.java
@@ -0,0 +1,10 @@
+package com.example.Exercise.controller;
+
+import org.springframework.stereotype.Component;
+
+@Component
+public class PizzaController {
+    public String getPizza(){
+        return "Hot Pizza";
+    }
+}
