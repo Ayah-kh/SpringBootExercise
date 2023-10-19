8db29a69f70684fc06e7879d6d3396b75fbd7562
Ayah.kh GitHub <ayah.java.dev@gmail.com>
2023-10-19 12:58:37 +0300

    First Commit

--- src/main/java/com/example/Exercise/controller/PizzaController.java
+++ src/main/java/com/example/Exercise/controller/PizzaController.java
@@ -1,10 +1,25 @@
 package com.example.Exercise.controller;
 
+import com.example.Exercise.srevice.VegPizza;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
 @Component
 public class PizzaController {
-    public String getPizza(){
-        return "Hot Pizza";
+
+    private VegPizza vegPizza;
+
+//    @Autowired
+//    public PizzaController (VegPizza vegPizza){
+//        this.vegPizza=vegPizza;
+//    }
+
+    @Autowired
+    public void setVegPizza(VegPizza vegPizza) {
+        this.vegPizza = vegPizza;
+    }
+
+    public String getPizza() {
+        return vegPizza.getPizza();
     }
 }
