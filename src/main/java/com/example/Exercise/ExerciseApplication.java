159f264dd8febf2e174745d2363049e3b6ed26db
Ayah.kh GitHub <ayah.java.dev@gmail.com>
2023-10-19 12:54:59 +0300

    First Commit

--- src/main/java/com/example/Exercise/ExerciseApplication.java
+++ src/main/java/com/example/Exercise/ExerciseApplication.java
@@ -1,14 +1,19 @@
 package com.example.Exercise;
 
+import com.example.Exercise.controller.PizzaController;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ConfigurableApplicationContext;
 
 
 @SpringBootApplication
 public class ExerciseApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(ExerciseApplication.class, args);
-    }
+		ConfigurableApplicationContext context
+				= SpringApplication.run(ExerciseApplication.class, args);
+		PizzaController pizzaController =(PizzaController) context.getBean("pizzaController");
+		System.out.println(pizzaController.getPizza());
+	}
 
 }
---
+++ src/main/java/com/example/Exercise/srevice/VegPizza.java
@@ -0,0 +1,11 @@
+package com.example.Exercise.srevice;
+
+import org.springframework.stereotype.Component;
+
+@Component
+public class VegPizza {
+
+    public String getPizza(){
+        return  "VegPizza";
+    }
+}
