2e3152b25960737431df794a10b1c6c385f9bcbe
Ayah.kh GitHub <ayah.java.dev@gmail.com>
2023-10-22 12:43:12 +0300

    First Commit

--- src/main/java/com/example/Exercise/controller/BookController.java
+++ src/main/java/com/example/Exercise/controller/BookController.java
@@ -4,18 +4,22 @@ import com.example.Exercise.beans.Book;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
 
-@Controller
+//@Controller
+//@ResponseBody
+@RestController
+@RequestMapping("/api")
 public class BookController {
 
     @RequestMapping("/hello-world")
-    @ResponseBody
+//    @ResponseBody
     public String sayHelloWorld(){
         return "Hello World";
     }
 
     @RequestMapping("/book")
-    @ResponseBody
+//    @ResponseBody
     public Book getBook(){
         Book book= new Book(1,"Core Java","Learn Core JAva");
         return book;
