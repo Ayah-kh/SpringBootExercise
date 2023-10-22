47943c3e08b51f922983fd9f58834e7912e06cae
Ayah.kh GitHub <ayah.java.dev@gmail.com>
2023-10-22 13:47:44 +0300

    First Commit

--- src/main/java/com/example/Exercise/controller/BookController.java
+++ src/main/java/com/example/Exercise/controller/BookController.java
@@ -1,7 +1,9 @@
 package com.example.Exercise.controller;
 
 import com.example.Exercise.beans.Book;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
 
@@ -26,6 +28,23 @@ public class BookController {
     public Book getBook(){
         Book book= new Book(1,"Core Java","Learn Core JAva");
         return book;
+    }
+
+//    @ResponseStatus(value = HttpStatus.CREATED)
+    @PostMapping(value = "/books/create")
+    public ResponseEntity<Book> createBook(@RequestBody Book book){
+        System.out.println(book.getId());
+        System.out.println(book.getBookTitle());
+        System.out.println(book.getDescription());
+        return new ResponseEntity<>(book,HttpStatus.CREATED);
+    }
+
+    @PutMapping ("/books/update/{id}")
+    public ResponseEntity<Book> update(int id,Book updatedBook){
+        System.out.println(id);
+        System.out.println(updatedBook.getBookTitle());
+        System.out.println(updatedBook.getDescription());
+        return new ResponseEntity<>(updatedBook,HttpStatus.OK);
     }
 
 }
